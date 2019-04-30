d5c71360b56e08f13970f8e7375df663c3e65cb9
hyunsong <kny4950@naver.com>
2019-04-30 19:29:55 +0900

    initial summoner-crawler

---
+++ src/main/java/org/cnu/realcoding/rc04/summonercrawler/SummonerCrawlerApplication.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding.rc04.summonercrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummonerCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummonerCrawlerApplication.class, args);
+	}
+
+}
