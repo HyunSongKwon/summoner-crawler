d5af7af19cf68a698363b9784628adab0cbdd8b6
zzz845zz <zzz845zz@gmafds>
2019-05-01 01:16:00 +0900

    add config class. 'SwaggerConfig'

---
+++ src/main/java/org/cnu/realcoding/rc04/summonercrawler/config/SwaggerConfig.java
@@ -0,0 +1,8 @@
+package org.cnu.realcoding.rc04.summonercrawler.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+public class SwaggerConfig {
+}
