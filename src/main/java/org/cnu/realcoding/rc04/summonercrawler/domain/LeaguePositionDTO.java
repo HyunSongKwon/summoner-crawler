a81c3dc2aa7e0c30ce148ebc415588b1809e0dda
zzz845zz <zzz845zz@gmafds>
2019-05-01 01:02:21 +0900

    add domain LeaguePositionDTO's data

--- src/main/java/org/cnu/realcoding/rc04/summonercrawler/domain/LeaguePositionDTO.java
+++ src/main/java/org/cnu/realcoding/rc04/summonercrawler/domain/LeaguePositionDTO.java
@@ -4,4 +4,19 @@ import lombok.Data;
 
 @Data
 public class LeaguePositionDTO {
+    String queueType;
+    String summonerName;
+    boolean hotStreak;
+    int wins;
+    boolean veteran;
+    int looses;
+    String rank;
+    String leagueId;
+    boolean inactive;
+    boolean freshBlood;
+    String leagueName;
+    String position;
+    String tier;
+    String summonerId;
+    int leaguePoints;
 }
