b926519b91525cb491934c1a0aa1304cd4907579
jaehong0612 <jaehong0612@gmail.com>
2019-05-01 22:18:22 +0900

    rename repository

--- src/main/java/org/cnu/realcoding/rc04/summonercrawler/repository/SummonerRepository.java
+++ src/main/java/org/cnu/realcoding/rc04/summonercrawler/repository/SummonerRepository.java
@@ -1,11 +1,12 @@
 package org.cnu.realcoding.rc04.summonercrawler.repository;
 
+import org.cnu.realcoding.rc04.summonercrawler.domain.SummonerDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class SummonerDTO {
+public class SummonerRepository {
     @Autowired
     private MongoTemplate mongoTemplate;
 
